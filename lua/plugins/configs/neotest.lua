d21242ae03adb9edbed283507b0287bee89252c7
Jonny Gamba <jonny.gamba@deliveryhero.com>
2022-11-18 09:48:23 -0500

    remove mappings from neotest

--- lua/plugins/configs/neotest.lua
+++ lua/plugins/configs/neotest.lua
@@ -15,7 +15,3 @@ require('neotest').setup({
     unknown = ""
   }
 })
-vim.keymap.set('n', '<Leader>t1', "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>")
-vim.keymap.set('n', '<Leader>t2', "<cmd>lua require('neotest').run.run()<CR>")
-vim.keymap.set('n', '<Leader>t3', "<cmd>lua require('neotest').summary.toggle()<CR>")
-vim.keymap.set('n', '<Leader>t4', "<cmd>lua require('neotest').output.open()<CR>")
