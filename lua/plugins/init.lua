7c7106ee8eaa4fa92d8cb98d917b79f18df1e567
Jonny Gamba <88061915+gambonny@users.noreply.github.com>
2022-11-23 12:23:17 -0500

    add scrollbar colors

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -133,6 +133,11 @@ local packer = require('packer').startup({ function(use)
         handlers = {
           gitsigns = true,
         },
+        marks = {
+          GitAdd = { color = "#266d6a" },
+          GitChange = { color = "#536c9e" },
+          GitDelete = { color = "#b2555b" },
+        }
       })
     end
   })
